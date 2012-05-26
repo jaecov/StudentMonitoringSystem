4e482aabba717ee3aab07047cb1c369d767d2914
jaecov <jaecov@hotmail.com>
2012-05-26 16:54:06 +0800

    updated
    Signed-off-by: jaecov <jaecov@hotmail.com>

--- Database/SMS_May 26, 2012.sql
+++ Database/SMS_May 26, 2012.sql
@@ -54,8 +54,8 @@ CREATE TABLE `employees_section` (
   `ModifiedDate` datetime DEFAULT NULL,
   KEY `FK_guardiansstudent` (`EmployeeID`),
   KEY `FK_guardiansstudents` (`SectionID`),
-  CONSTRAINT `FK_employeessections` FOREIGN KEY (`SectionID`) REFERENCES `sections` (`SectionID`),
-  CONSTRAINT `FK_employeessection` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`)
+  CONSTRAINT `FK_employeessection` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`),
+  CONSTRAINT `FK_employeessections` FOREIGN KEY (`SectionID`) REFERENCES `sections` (`SectionID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `employees_section` */
@@ -72,8 +72,11 @@ CREATE TABLE `guardians` (
   `Birthday` date DEFAULT NULL,
   `ImagePath` varchar(500) DEFAULT NULL,
   `EmailAddress` varchar(100) DEFAULT NULL,
-  `RFID` varchar(100) DEFAULT NULL,
+  `RFID` varchar(100) DEFAULT NULL COMMENT 'RFID hex code',
   `Note` varchar(500) DEFAULT NULL,
+  `AutomaticSMS` tinyint(1) NOT NULL COMMENT 'if True, system will include the number as recepient',
+  `AutomaticEmail` tinyint(1) NOT NULL COMMENT 'if True, system will include the email as recepient',
+  `Active` tinyint(1) NOT NULL COMMENT 'Flag as soft delete',
   `CreatedBy` int(11) NOT NULL,
   `CreatedDate` datetime NOT NULL,
   `ModifiedBy` int(11) DEFAULT NULL,
@@ -114,8 +117,8 @@ CREATE TABLE `guardians_student` (
   `ModifiedDate` datetime DEFAULT NULL,
   KEY `FK_guardiansstudent` (`GuardianID`),
   KEY `FK_guardiansstudents` (`StudentID`),
-  CONSTRAINT `FK_guardiansstudents` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`),
-  CONSTRAINT `FK_guardiansstudent` FOREIGN KEY (`GuardianID`) REFERENCES `guardians` (`GuardianID`)
+  CONSTRAINT `FK_guardiansstudent` FOREIGN KEY (`GuardianID`) REFERENCES `guardians` (`GuardianID`),
+  CONSTRAINT `FK_guardiansstudents` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `guardians_student` */
@@ -126,15 +129,17 @@ DROP TABLE IF EXISTS `sections`;
 
 CREATE TABLE `sections` (
   `SectionID` int(11) NOT NULL AUTO_INCREMENT,
-  `SectionName` varchar(100) DEFAULT NULL,
-  `CreatedBy` int(11) DEFAULT NULL,
-  `CreatedDate` datetime DEFAULT NULL,
+  `SectionName` varchar(100) NOT NULL,
+  `CreatedBy` int(11) NOT NULL,
+  `CreatedDate` datetime NOT NULL,
   `ModifiedBy` int(11) DEFAULT NULL,
   `ModifiedDate` datetime DEFAULT NULL,
   PRIMARY KEY (`SectionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `sections` */
+
+insert  into `sections`(`SectionID`,`SectionName`,`CreatedBy`,`CreatedDate`,`ModifiedBy`,`ModifiedDate`) values (1,'P1',1,'2012-01-01 00:00:00',NULL,NULL),(2,'P2',1,'2012-01-01 00:00:00',NULL,NULL),(3,'P3',1,'2012-01-01 00:00:00',NULL,NULL);
 
 /*Table structure for table `sms_guardians` */
 
@@ -234,11 +239,11 @@ CREATE TABLE `students` (
   `ImagePath` varchar(500) DEFAULT NULL,
   `EmailAddress` varchar(100) DEFAULT NULL,
   `SectionID` int(11) DEFAULT NULL,
-  `RFID` varchar(100) DEFAULT NULL,
+  `RFID` varchar(100) DEFAULT NULL COMMENT 'RFID hex code',
   `Note` varchar(500) DEFAULT NULL,
-  `Active` tinyint(1) NOT NULL,
-  `CreatedBy` int(11) DEFAULT NULL,
-  `CreatedDate` datetime DEFAULT NULL,
+  `Active` tinyint(1) NOT NULL COMMENT 'flag as soft delete',
+  `CreatedBy` int(11) NOT NULL,
+  `CreatedDate` datetime NOT NULL,
   `ModifiedBy` int(11) DEFAULT NULL,
   `ModifiedDate` datetime DEFAULT NULL,
   PRIMARY KEY (`StudentID`),
@@ -248,7 +253,7 @@ CREATE TABLE `students` (
 
 /*Data for the table `students` */
 
-insert  into `students`(`StudentID`,`StudentNumber`,`FirstName`,`MiddleName`,`LastName`,`Birthday`,`ImagePath`,`EmailAddress`,`SectionID`,`RFID`,`Note`,`Active`,`CreatedBy`,`CreatedDate`,`ModifiedBy`,`ModifiedDate`) values (1,'087-2010-0020','Gladys','Cabuhat','Valeriano','1989-08-09',NULL,NULL,NULL,NULL,'Very Good',0,NULL,NULL,NULL,NULL),(2,'087-2010-0022','Gemma Rica','Santiago','Chua','1990-11-16',NULL,NULL,NULL,NULL,':D Good :D',0,NULL,NULL,NULL,NULL),(3,'087-2010-0009','Jaypee','Dela Cruz','Porciunculla','1988-10-01',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(4,'087-2010-0023','Jun','Garces','Gialogo','1987-06-17',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(5,'087-2010-0025','Roxanne','Obliosca','Banluta','1991-11-11',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(6,'087-2010-0008','Ryam','C','Fernando','1989-11-22',NULL,NULL,NULL,NULL,'Very Good',0,NULL,NULL,NULL,NULL),(7,'087-2010-0011','Emilio','Parfahn','Villanueva','1988-09-05',NULL,NULL,NULL,NULL,'Good',0,NULL,NULL,NULL,NULL),(8,'087-2010-0030','Aive','D','Kalos','2010-10-24',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(9,'087-2010-0042','Gorgonio','Jombo','Cruz','1987-02-04',NULL,NULL,NULL,NULL,'SAMPLE',0,NULL,NULL,NULL,NULL),(10,'087-2010-0004','Jasmine','Dsa','Torno','1984-09-30',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(11,'087-2010-0007','John Carlo','Delos Reyes','Cruz','1987-04-14',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(12,'087-2010-0014','Marie Christine','Quinzon','Ventura','2010-09-24',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(13,'087-2010-0017','Ronald','Acuesta','Trinidad','1990-04-20',NULL,NULL,NULL,NULL,'Good',0,NULL,NULL,NULL,NULL),(14,'087-2010-0026','Alan','L','Perjes','1988-04-28',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(15,'087-2010-0024','Arianne Giselle','Abaya','Soberano','1990-09-20',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(16,'087-2010-0027','Vince','S','Abosolo','1988-10-23',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(17,'087-2010-0029','Moniena','Moreno','Santiago','1993-12-14',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(18,'087-2010-0003','Maan','S.','Talabia','1983-09-30',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(19,'087-2010-0005','Jaja','Santiago','Vasquez','1988-10-29',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(20,'087-2010-0010','Iris Erika','Reynaldo','Reyes','1986-09-06',NULL,NULL,NULL,NULL,'very good',0,NULL,NULL,NULL,NULL),(21,'087-2010-0012','Karla Chriselda','Avendaño','Castillio','1988-04-14',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(22,'087-2010-0018','Myra','Casidsid','Flores','1989-11-01',NULL,NULL,NULL,NULL,'Good',0,NULL,NULL,NULL,NULL),(23,'087-2010-0019','Krisanto','Perceverancia','Rey','1990-03-18',NULL,NULL,NULL,NULL,'record of theft and jailed once',0,NULL,NULL,NULL,NULL),(24,'087-2010-0002','Jaecov','Santiago','Vasquez','1987-05-04',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(25,'087-2010-0006','Ma. Lou-Anne','Yao','Dino','1988-09-21',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(26,'087-2010-0016','Mark William','T','Zafra','1987-05-31',NULL,NULL,NULL,NULL,'Good',0,NULL,NULL,NULL,NULL),(27,'087-2010-0015','Nazer','Santiago','Miranda','1987-07-18',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL),(28,'087-2010-0013','Ruby','Palallos','Palma','1990-09-27',NULL,NULL,NULL,NULL,'very good',0,NULL,NULL,NULL,NULL),(29,'087-2012-0044','King','Qwdqwdcqwrw','Wdqwdqw','1986-05-16',NULL,NULL,NULL,NULL,'',0,NULL,NULL,NULL,NULL);
+insert  into `students`(`StudentID`,`StudentNumber`,`FirstName`,`MiddleName`,`LastName`,`Birthday`,`ImagePath`,`EmailAddress`,`SectionID`,`RFID`,`Note`,`Active`,`CreatedBy`,`CreatedDate`,`ModifiedBy`,`ModifiedDate`) values (1,'087-2010-0020','Gladys','Cabuhat','Valeriano','1989-08-09',NULL,NULL,1,NULL,'Very Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(2,'087-2010-0022','Gemma Rica','Santiago','Chua','1990-11-16',NULL,NULL,1,NULL,':D Good :D',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(3,'087-2010-0009','Jaypee','Dela Cruz','Porciunculla','1988-10-01',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(4,'087-2010-0023','Jun','Garces','Gialogo','1987-06-17',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(5,'087-2010-0025','Roxanne','Obliosca','Banluta','1991-11-11',NULL,NULL,2,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(6,'087-2010-0008','Ryam','C','Fernando','1989-11-22',NULL,NULL,1,NULL,'Very Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(7,'087-2010-0011','Emilio','Parfahn','Villanueva','1988-09-05',NULL,NULL,2,NULL,'Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(8,'087-2010-0030','Aive','D','Kalos','2010-10-24',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(9,'087-2010-0042','Gorgonio','Jombo','Cruz','1987-02-04',NULL,NULL,2,NULL,'SAMPLE',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(10,'087-2010-0004','Jasmine','Dsa','Torno','1984-09-30',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(11,'087-2010-0007','John Carlo','Delos Reyes','Cruz','1987-04-14',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(12,'087-2010-0014','Marie Christine','Quinzon','Ventura','2010-09-24',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(13,'087-2010-0017','Ronald','Acuesta','Trinidad','1990-04-20',NULL,NULL,1,NULL,'Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(14,'087-2010-0026','Alan','L','Perjes','1988-04-28',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(15,'087-2010-0024','Arianne Giselle','Abaya','Soberano','1990-09-20',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(16,'087-2010-0027','Vince','S','Abosolo','1988-10-23',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(17,'087-2010-0029','Moniena','Moreno','Santiago','1993-12-14',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(18,'087-2010-0003','Maan','S.','Talabia','1983-09-30',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(19,'087-2010-0005','Jaja','Santiago','Vasquez','1988-10-29',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(20,'087-2010-0010','Iris Erika','Reynaldo','Reyes','1986-09-06',NULL,NULL,1,NULL,'very good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(21,'087-2010-0012','Karla Chriselda','Avendaño','Castillio','1988-04-14',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(22,'087-2010-0018','Myra','Casidsid','Flores','1989-11-01',NULL,NULL,1,NULL,'Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(23,'087-2010-0019','Krisanto','Perceverancia','Rey','1990-03-18',NULL,NULL,1,NULL,'record of theft and jailed once',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(24,'087-2010-0002','Jaecov','Santiago','Vasquez','1987-05-04',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(25,'087-2010-0006','Ma. Lou-Anne','Yao','Dino','1988-09-21',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(26,'087-2010-0016','Mark William','T','Zafra','1987-05-31',NULL,NULL,1,NULL,'Good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(27,'087-2010-0015','Nazer','Santiago','Miranda','1987-07-18',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(28,'087-2010-0013','Ruby','Palallos','Palma','1990-09-27',NULL,NULL,1,NULL,'very good',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00'),(29,'087-2012-0044','King','Qwdqwdcqwrw','Wdqwdqw','1986-05-16',NULL,NULL,1,NULL,'',1,0,'2012-01-01 00:00:00',NULL,'2012-01-01 00:00:00');
 
 /*Table structure for table `students_mobile` */
 
